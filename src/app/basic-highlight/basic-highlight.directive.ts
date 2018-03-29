df36e858961bbc86fc2a67f5eefba7392270c5bb
Nathan Pickard <nathan.pickard87@gmail.com>
2018-03-28 22:10:02 -0700

    Using the Angular Renderer class to change DOM element styling

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -6,6 +6,7 @@ import { Directive, ElementRef, OnInit } from "@angular/core";
 export class BasicHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef) {
   }
+  
   ngOnInit() {
     this.elementRef.nativeElement.style.backgroundColor = 'green';
   }
